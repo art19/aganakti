6223745782e46a366f3d41f04d7aee0ab63cddb2
Keith Gable <keith@art19.com>
2021-03-17 15:30:27 -0700

    Add a mixin for delegations

---
+++ lib/aganakti/query/delegations.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'active_support/core_ext/module/delegation'
+
+module Aganakti
+  class Query
+    ##
+    # Mixes in delegations so the query quacks like an +ActiveRecord::Result+
+    #
+    # @see https://api.rubyonrails.org/classes/ActiveRecord/Result.html ActiveRecord::Result
+    module Delegations
+      ##
+      # @!macro [attach] activerecord_result_delegator
+      #   @!method $1
+      #     Delegates to <tt>$2.$1</tt>
+      #
+      #     @see ActiveRecord::Result#$1
+      delegate '[]', to: :result
+      delegate :columns, to: :result
+      delegate :column_types, to: :result
+      delegate :each, to: :result
+      delegate :includes_column?, to: :result
+      delegate :last, to: :result
+      delegate :length, to: :result
+      delegate :map, to: :result
+      delegate :empty?, to: :result
+      delegate :rows, to: :result
+      delegate :to_ary, to: :result
+      delegate :to_a, to: :result
+    end
+  end
+end
