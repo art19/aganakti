c5ac2065a795cbfc516718c5d1e67826a595651d
Keith Gable <keith@art19.com>
2021-03-22 16:39:29 -0700

    Clean up the result parser

--- lib/aganakti/query/result_parser.rb
+++ lib/aganakti/query/result_parser.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-require 'json'
+require 'active_support/deprecation'
+require 'active_record/result'
+require 'oj'
 
 module Aganakti
   class Query
@@ -9,47 +11,65 @@ module Aganakti
     # {Aganakti::Query}; you should not need to use this directly.
     #
     # @api private
-    module ResultParser
-      ##
-      # Parses the successful response body. Note that no checking is performed for the server returning
-      # a different number of columns in the header vs. in the result. This shouldn't happen and would
-      # be a parser issue, but the parser checks for every possibly weird situation and so this sacrifice
-      # was made for performance.
-      #
-      # @param resp [Typhoeus::Response] the response object
-      # @return [ActiveRecord::Result] the result set
-      # @raise [Aganakti::QueryResultTruncatedError] if the query result is incomplete and can't be trusted
-      def parse_response(resp)
-        lines = resp.body.lines
+    class ResultParser
+      class << self
+        ##
+        # Parses the successful response body. Note that no checking is performed for the server returning
+        # a different number of columns in the header vs. in the result. This shouldn't happen and would
+        # be a parser issue, but the parser checks for every possibly weird situation and so this sacrifice
+        # was made for performance.
+        #
+        # @param resp [Typhoeus::Response] the response object
+        # @return [ActiveRecord::Result] the result set
+        # @raise [Aganakti::QueryResultTruncatedError] if the query result is incomplete and can't be trusted
+        def parse_response(resp)
+          lines = resp.body.lines
 
-        raise QueryResultTruncatedError unless lines.last == "\n"
+          raise QueryResultTruncatedError unless lines.last == "\n"
 
-        rows    = lines.reject { |line| line == "\n" }.map(&RowParser.method(:parse))
-        columns = rows.shift
+          rows    = lines.reject { |line| line == "\n" }.map(&RowParser.method(:parse))
+          columns = rows.shift
 
-        ActiveRecord::Result.new(columns, rows.freeze)
-      end
-      module_function :parse_response
+          ActiveRecord::Result.new(columns, rows.freeze)
+        end
 
-      ##
-      # Validates the Typhoeus response to determine if we should continue parsing it
-      #
-      # @param resp [Typhoeus::Response] the response
-      # @raise [Aganakti::QueryError] if either a cURL error occurred or the server was unable to handle the query
-      # @raise [Aganakti::QueryTimedOutError] if the query timed out before being able to be executed
-      def validate_response!(resp)
-        raise QueryTimedOutError if resp.timed_out?
-        raise QueryError, "cURL error #{resp.return_code}: #{resp.return_message}" if resp.code.zero?
-        return if resp.code == 200
+        ##
+        # Validates the Typhoeus response to determine if we should continue parsing it
+        #
+        # @param resp [Typhoeus::Response] the response
+        # @raise [Aganakti::QueryError] if either a cURL error occurred or the server was unable to handle the query
+        # @raise [Aganakti::QueryTimedOutError] if the query timed out before being able to be executed
+        def validate_response!(resp)
+          return if resp.code == 200 # this is intentionally not .success? because all other status codes are unexpected
 
-        begin
-          error_info = JSON.parse(resp.body)
-          raise QueryError, [error_info['error'], error_info['errorMessage']].compact!.join(': ')
-        rescue StandardError
-          raise QueryError, "An error occurred, but the server's response was unparseable"
+          raise QueryTimedOutError if resp.timed_out?
+          raise QueryError, "cURL error #{resp.return_code}: #{resp.return_message}" if resp.code.zero?
+
+          raise QueryError, parse_query_error(resp.body)
+        end
+
+        private
+
+        ##
+        # Given a response body, parse an error message from it
+        #
+        # @param body [String] the response body with errors inside
+        # @return [String] the error message
+        def parse_query_error(body)
+          error_msg = nil
+
+          begin
+            error_info = Oj.load(body, mode: :strict)
+
+            error_msg_components = [error_info['error'], error_info['errorMessage']].compact
+            error_msg = error_msg_components.join(': ') unless error_msg_components.empty?
+          ensure
+            error_msg ||= "An error occurred, but the server's response was unparseable: #{body}"
+          end
+
+          error_msg
         end
       end
-      module_function :validate_response!
     end
   end
 end
