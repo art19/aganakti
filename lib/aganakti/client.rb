4fc507f882a72e98ea89613045541878f11a046c
Keith Gable <keith@art19.com>
2021-03-23 13:01:57 -0700

    More docs

--- lib/aganakti/client.rb
+++ lib/aganakti/client.rb
@@ -9,6 +9,12 @@ module Aganakti
   # An instance of a Druid SQL client. Because Typhoeus/libcurl handle all thread and process safety issues, it is not necessary to create a
   # new client per thread/process (e.g., in a Unicorn after_fork). The client is intended to be constructed by {Aganakti.new}, which abstracts
   # some of the more complicated cURL options. You're welcome to construct this class instead if you need to do something special.
+  #
+  # The main method on this class is {#query}, which allows you to construct an optionally parameterized query.
+  #
+  # This class additionally has some helpers for escaping when parameterized queries are unavailable or are unsupported in the
+  # situation you're in (e.g., +INTERVAL '5' MONTHS+ cannot use a parameter for +'5'+). These methods are on the client instance rather than
+  # the client class to provide for the possibility of server-specific escaping rules in the future.
   class Client
     ##
     # @api private
