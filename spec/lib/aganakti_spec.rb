f45f9faf578c4f58e7f31a267c67596b276139d4
Keith Gable <keith@art19.com>
2021-03-24 18:46:17 -0700

    Make the RSpec documentation format nicer by properly nesting the ::VERSION specs

--- spec/lib/aganakti_spec.rb
+++ spec/lib/aganakti_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe Aganakti do
-  it { expect(described_class::VERSION).not_to be nil }
-  it { expect(described_class::VERSION).to be_frozen }
+  describe '::VERSION' do
+    it { expect(described_class::VERSION).not_to be nil }
+    it { expect(described_class::VERSION).to be_frozen }
+  end
 end
