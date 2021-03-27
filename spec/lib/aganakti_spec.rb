ed1dcf3e3149992b7ba9a7f8d650934e1dabe7df
Keith Gable <keith@art19.com>
2021-03-26 22:23:51 -0700

    Add specs for the errors

--- spec/lib/aganakti_spec.rb
+++ spec/lib/aganakti_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe Aganakti do
     it { expect(described_class::VERSION).not_to be nil }
     it { expect(described_class::VERSION).to be_frozen }
   end
+
+  describe '::Error' do
+    it 'is a subclass of StandardError' do
+      expect(described_class::Error).to(satisfy { |c| c < StandardError })
+    end
+  end
+
+  %i[
+    ConfigurationError IllegalEscapeError QueryAlreadyExecutedError QueryResultTruncatedError
+    QueryResultUnparseableError QueryTimedOutError QueryError
+  ].each do |err|
+    describe "::#{err}" do
+      it 'is a subclass of Aganakti::Error' do
+        expect(described_class.const_get(err)).to(satisfy { |c| c < described_class::Error })
+      end
+    end
+  end
 end
