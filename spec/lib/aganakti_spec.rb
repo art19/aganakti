9e5318b78fa17eb4a8c24c70b4690861d546fad0
Keith Gable <keith@art19.com>
2021-04-06 15:30:37 -0700

    Spec construction with a basic HTTP/HTTPS URL and defaults

--- spec/lib/aganakti_spec.rb
+++ spec/lib/aganakti_spec.rb
@@ -25,19 +25,73 @@ RSpec.describe Aganakti do
 
   describe '.new' do
     context 'with an invalid URI' do
-      pending
+      it 'raises a URI::InvalidURIError' do
+        expect { described_class.new('is it pronounced hydro city zone or hydrossity zone?') }.to raise_error(URI::InvalidURIError)
+      end
     end
 
     context 'with a FTP URI' do
-      pending
+      it 'raises an Aganakti::ConfigurationError' do
+        expect { described_class.new('ftp://segapr.segaamerica.com/SEGA_ARCHIVES/Dreamcast_Games/Seaman/seam7.jpg') }
+          .to raise_error(Aganakti::ConfigurationError, 'URI must be a HTTP or HTTPS URI')
+      end
     end
 
-    context 'with a HTTP URI' do
-      pending
-    end
+    %w[HTTP HTTPS].each do |proto|
+      context "with a #{proto} URI", :stubbed_client do
+        let(:url) { "#{proto.downcase}://druidserver/query" }
 
-    context 'with a HTTPS URI' do
-      pending
+        it 'created a client with accept_encoding = ""' do
+          described_class.new(url)
+
+          expect(Aganakti::Client).to have_received(:new).with(anything, hash_including(accept_encoding: ''))
+        end
+
+        it 'created a client with the Connection header set to "keep-alive"' do
+          described_class.new(url)
+
+          expect(Aganakti::Client).to have_received(:new).with(anything, hash_including(headers: hash_including('Connection' => 'keep-alive')))
+        end
+
+        it 'created a client with the expected URL' do
+          described_class.new(url)
+
+          expect(Aganakti::Client).to have_received(:new).with(url, anything)
+        end
+
+        it 'created a client with the User-Agent header set to the default' do
+          described_class.new(url)
+
+          expect(Aganakti::Client).to have_received(:new).with(anything, hash_including(
+                                                                           headers: hash_including(
+                                                                             # not matching beyond libcurl because it depends on the built libcurl
+                                                                             'User-Agent' => %r{\AAganakti/[\w.]+ Typhoeus/[\w.]+ Ruby/[\w.]+ libcurl/.*}
+                                                                           )
+                                                                         ))
+        end
+
+        it 'created a client without cainfo' do
+          described_class.new(url)
+
+          expect(Aganakti::Client).to have_received(:new).with(anything, hash_including(cainfo: nil))
+        end
+
+        it 'created a client without connecttimeout' do
+          described_class.new(url)
+
+          expect(Aganakti::Client).to have_received(:new).with(anything, hash_including(connecttimeout: nil))
+        end
+
+        it 'created a client without timeout' do
+          described_class.new(url)
+
+          expect(Aganakti::Client).to have_received(:new).with(anything, hash_including(timeout: nil))
+        end
+
+        it "doesn't raise an error" do
+          expect { described_class.new(url) }.not_to raise_error
+        end
+      end
     end
 
     context 'with a specified but missing CA bundle' do
